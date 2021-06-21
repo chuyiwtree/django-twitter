95a4c1d884863ca77f8b39086c21dc68a9149252
chuyi <chuyiwtree@gmail.com>
2021-06-21 12:36:32 -0700

    newsfeeds-endless-pagination

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -6,34 +6,39 @@ from newsfeeds.models import NewsFeed
 from newsfeeds.api.serializers import NewsFeedSerializer
 
 
-class NewsFeedViewSet(viewsets.GenericViewSet):
-    permission_classes = [IsAuthenticated]
-    queryset = NewsFeed.objects.all()
-
-    def list(self, request):
-        newsfeeds = NewsFeed.objects.filter(user=request.user)
-        serializer = NewsFeedSerializer(
-            newsfeeds,
-            context={'request': request},
-            many=True,
-        )
-        return Response({
-            'newsfeeds': serializer.data,
-        }, status=status.HTTP_200_OK)
-
-
 # class NewsFeedViewSet(viewsets.GenericViewSet):
 #     permission_classes = [IsAuthenticated]
-#
-#     def get_queryset(self):
-#         # 自定义 queryset，因为 newsfeed 的查看是有权限的
-#         # 只能看 user=当前登录用户的 newsfeed
-#         # 也可以是 self.request.user.newsfeed_set.all()
-#         # 但是一般最好还是按照 NewsFeed.objects.filter 的方式写，更清晰直观
-#         return NewsFeed.objects.filter(user=self.request.user)
+#     queryset = NewsFeed.objects.all()
 #
 #     def list(self, request):
-#         serializer = NewsFeedSerializer(self.get_queryset(), many=True)
+#         newsfeeds = NewsFeed.objects.filter(user=request.user)
+#         serializer = NewsFeedSerializer(
+#             newsfeeds,
+#             context={'request': request},
+#             many=True,
+#         )
 #         return Response({
 #             'newsfeeds': serializer.data,
 #         }, status=status.HTTP_200_OK)
+from utils.paginations import EndlessPagination
+
+
+class NewsFeedViewSet(viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    pagination_class = EndlessPagination
+
+    def get_queryset(self):
+        # 自定义 queryset，因为 newsfeed 的查看是有权限的
+        # 只能看 user=当前登录用户的 newsfeed
+        # 也可以是 self.request.user.newsfeed_set.all()
+        # 但是一般最好还是按照 NewsFeed.objects.filter 的方式写，更清晰直观
+        return NewsFeed.objects.filter(user=self.request.user)
+
+    def list(self, request):
+        queryset = self.paginate_queryset(self.get_queryset())
+        serializer = NewsFeedSerializer(
+            queryset,
+            context={'request': request},
+            many=True,
+        )
+        return self.get_paginated_response(serializer.data)
