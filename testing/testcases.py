0688eb4015c929410c3772f5cda0d3cf0dc8eb54
chuyi <chuyiwtree@gmail.com>
2021-06-24 10:08:05 -0700

    add-memcached

--- testing/testcases.py
+++ testing/testcases.py
@@ -1,4 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
+from django.core.cache import caches
 from django.test import TestCase as DjangoTestCase
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
@@ -10,6 +11,9 @@ from tweets.models import Tweet
 
 
 class TestCase(DjangoTestCase):
+
+    def clear_cache(self):
+        caches['testing'].clear()
 
     @property
     def anonymous_client(self):
